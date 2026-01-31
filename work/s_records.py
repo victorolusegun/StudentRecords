90d5d073c81b3c30ca19e12b6179342d6ccab2e5
Victor Olusegun <victorolusegun003@gmail.com>
2026-01-31 10:32:54 +0100

    Ability to add multiple students and some bug fixes

--- work/s_records.py
+++ work/s_records.py
@@ -1,11 +1,36 @@
 print('Welcome')
 
-# def val_input(student):
-    
+# Functions
+def string_validation(variable):
+    while variable == '' or variable.isnumeric() is True:
+        variable = input('Invalid input.\nEnter valid input:\n')
+    return variable
+
+def int_validation(variable):
+    while variable == '' or variable.isnumeric() is False:
+        variable = input('Invalid input.\nEnter valid input:\n')
+    return variable
+
 records = {}
 
-student = input('Enter student name: \n')
-while student == '' or student.isnumeric() is True:
-    student = input('Name can not be empty or numeric. Enter valid name: \n')
-student = student.capitalize()
-print(f'Hello, {student}!')
+try:
+    total = int(input('Enter number of students you wish to enter:\n'))
+except ValueError:
+    total = 1
+counter = 0
+while counter < total:
+    # Collect student name and validate input
+    student = input('Enter student name (Enter exit to stop):\n')
+    student = string_validation(student)
+    student = student.strip().capitalize()
+    if student == 'Exit':
+        break
+    
+    # Receive score and validate input
+    score = input(f'Enter {student} score:\n')
+    score = int_validation(score)
+    print(f'{student} scored {score}')
+
+    # Store in dictionary
+    records[student] = score
+    counter += 1
--- work/trial.py
+++ work/trial.py
@@ -1,1 +1,4 @@
-score = int(input('Enter score:\n'))
+try:
+    score = int(input('Enter number: \n'))
+except ValueError:
+    score = int(input('Invalid input. Enter a valid number: \n'))
