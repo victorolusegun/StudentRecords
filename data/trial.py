de006e53b070386796cd9aa86254e734251232c3
Victor Olusegun <victorolusegun003@gmail.com>
2026-02-01 20:44:39 +0100

    added option to update scores and few statistics

--- data/trial.py
+++ data/trial.py
@@ -1,3 +1,5 @@
-test = {'name': 'Alice', 'score': 95}
-
-print(test['name'])
+stats_opt = ['Number of students', 'Largest score', 'Smallest score', 'Average score']
+for index, options in enumerate(stats_opt):
+    print(f'{index + 1}. {options}')
+print('---------------------')
+input('What statistics would you like to see\n')
--- work/s_records.py
+++ work/s_records.py
@@ -6,9 +6,11 @@ def string_validation(variable):
         variable = input('Invalid input.\nEnter valid input:\n')
     return variable
 
-def int_validation(variable):
+def score_validation(variable):
     while variable == '' or variable.isnumeric() is False:
         variable = input('Invalid input.\nEnter valid input:\n')
+    while int(variable) < 0 or int(variable) > 100:
+        variable = input('Value should be in the range 0-100:\n')
     variable = int(variable)
     return variable
 
@@ -30,15 +32,36 @@ while counter < total:
 
     # Receive score and validate input
     score = input(f'Enter {student} score:\n')
-    score = int_validation(score)
+    score = score_validation(score)
     while score< 0 or score > 100:
         score = input('Value should be in the range 0-100:\n')
-        score = int_validation(score)
+        score = score_validation(score)
     print(f'{student} scored {score}')
 
     # Store in dictionary
     records[student] = score
     counter += 1
 
-for index, (key, values) in enumerate(records.items()):
-    print(f'{index+1}. {key} scored {values}')
+# View all records
+for index, (key, values) in enumerate(records.items(), start = 1):
+    print(f'{index}. {key} : {values}')
+
+# Update a record
+update = input('Do you want to update any record? (yes/no):\n')
+update = update.strip().lower()
+update = string_validation(update)
+if update == 'yes':
+    update_student = input('Enter student name to be updated:\n')
+    update_student = string_validation(update_student)
+    if len(records) == 0:
+        print('No record to update.')
+    elif update_student in records:
+        new_score = input(f'Enter {update_student} new score:\n')
+        new_score = score_validation(new_score)
+    elif update_student not in records:
+        print(f'{update_student} not found in records.')
+        
+records[update_student] = new_score
+
+for index, (key, values) in enumerate(records.items(), start = 1):
+    print(f'{index}. {key} : {values}')
