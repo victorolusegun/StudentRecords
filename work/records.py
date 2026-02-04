8443341c1e4efb98240f59cad0be8c78daafdec8
Victor Olusegun <victorolusegun003@gmail.com>
2026-02-04 18:27:56 +0100

    Got a class working

--- work/records.py
+++ work/records.py
@@ -1,3 +1,7 @@
-class Records:
+class StudentRecords:
     def __init__(self):
         self.records = {}
+    
+    def add_student(self, name, std_score):
+        self.records[name] = std_score
+    
--- work/s_records.py
+++ work/s_records.py
@@ -35,51 +35,55 @@ while counter < total:
     print(f'{student} scored {score}')
 
     # Store in dictionary
-    records = records.add_student(student,score)
+    records.add_student(student,score)
     counter += 1
 
 # View all records
-for index, (key, values) in enumerate(records.items(), start = 1):
+for index, (key, values) in enumerate(records.records.items(), start = 1):
     print(f'{index}. {key} : {values}')
 
+proceed = ['yes', 'no']
 # Update a record
 update = input('Do you want to update any record? (yes/no):\n')
 update = update.strip().lower()
 update = string_validation(update)
-if update == 'yes':
-    update_student = input('Enter student name to be updated:\n')
-    update_student = string_validation(update_student)
-    if len(records) == 0:
-        print('No record to update.')
-    elif update_student in records:
-        new_score = input(f'Enter {update_student} new score:\n')
-        new_score = score_validation(new_score)
-        records[update_student] = new_score
-    elif update_student not in records:
-        print(f'{update_student} not found in records.')
-else:
-    print('No update performed.')
-        
+while update not in proceed:
+    input('Invalid input, please enter (yes/no) ')
+if update in proceed:
+    if update == 'yes':
+        update_student = input('Enter student name to be updated:\n')
+        update_student = string_validation(update_student)
+        if len(records.records) == 0:
+            print('No record to update.')
+        elif update_student in records.records:
+            new_score = input(f'Enter {update_student} new score:\n')
+            new_score = score_validation(new_score)
+            records.add_student(update_student, new_score)
+        elif update_student not in records.records:
+            print(f'{update_student} not found in records.')
+    else:
+        print('No update performed.')
+            
 # Statistics. Extract scores from dictionary
 stats_opt = ['Number of students', 'Largest score', 'Smallest score', 'Average score']
 for index, options in enumerate(stats_opt, start = 1):
     print(f'{index}. {options}')
 print('---------------------')
-if records != {}:
+if records.records != {}:
     stats_input = input('What statistics would you like to see??\n')
     stats_input = score_validation(stats_input)
 
-    scores = records.values()
+    scores = records.records.values()
     score = list(scores)
 
     # Number of students
     if stats_input == 1:
-        no_of_students = len(records)
+        no_of_students = len(records.records)
         print(f'Number of students: {no_of_students}')
     elif stats_input == 2:
         # Max score
         max_score = max(score)
-        for key, value in records.items():
+        for key, value in records.records.items():
             if value == max_score:
                 high_std = key
         print(f'Highest score: {max_score} scored by {high_std}')
@@ -89,7 +93,7 @@ if records != {}:
         print(f'Lowest score: {min_score}')
     elif stats_input == 4:
         # Average score
-        avg_score = sum(score) / len(records)
+        avg_score = sum(score) / len(records.records)
         print(f'The average score is: {avg_score}')
     else:
         print(f'No available statistics')
