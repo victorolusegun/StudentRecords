dff7c417134b20c429c6d75054ed54cdf8286085
Victor Olusegun <victorolusegun003@gmail.com>
2026-03-03 08:53:55 +0100

    Updated view option

--- work/records.py
+++ work/records.py
@@ -5,3 +5,19 @@ class StudentRecords:
     def add_student(self, name, std_score):
         self.records[name] = std_score
     
+    def view_student(self):
+        if len(self.records) != 0:
+            for index, (key, values) in enumerate(self.records.items(), start = 1):
+                print(f'{index}. {key} : {values}')
+        else:
+            print('No available records to view.')
+    def update_student(self, name, new_score):
+        if len(self.records) == 0:
+            print('No record to update.')
+        elif name in self.records:
+            self.records[name] = new_score
+        else:
+            print(f'Student {name} not found in records.')
+
+class Statistics(StudentRecords):
+    pass
